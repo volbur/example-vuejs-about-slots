cb984416252b6e72fc6f77180bc7ce8e18f99042
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-06 08:56:14 +0300

    [App] conected all components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,36 @@
 <template>
-test
+  <div>
+    <the-header></the-header>
+    <!-- <TheHeader /> -->
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+  </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
 
 export default {
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
 };
 </script>
 
